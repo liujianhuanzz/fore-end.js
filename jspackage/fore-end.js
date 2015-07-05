7c8490cb14aa0a81332eb57beb778106c98f35de
liujianhuanzz <jianhuan0622@csu.edu.cn>
2015-07-05 16:51:23 +0800

    调整代码格式，规范化

--- jspackage/fore-end.js
+++ jspackage/fore-end.js
@@ -1,27 +1,27 @@
 /*
 采用jQuery插件的方式
 */
-(function($){
-	$.fn.fe_draggable = function(object){//拖拽实例化方法
+(function ($) {
+	$.fn.fe_draggable = function (object) {//拖拽实例化方法
 
-		var handler = object === undefined ? this : object['handler'];
-		var handlerEle = $(handler);
+		var handler = object === undefined ? this : object['handler'], 
+		    handlerEle = $(handler);
 
-		$.each(this,function(index, el) {
+		$.each(this, function (index, el) {
 			handlerEle[index].style.cursor = 'move';
-			createObject().drag(this,handlerEle[index]);
+			createObject().drag(this, handlerEle[index]);
 		});
 
 	};
 
-	$.fe_draggable = function(object){//拖拽静态方法
-		var dragEle = object['element'];
-		var handlerEle = object['handler'];
+	$.fe_draggable = function (object) {//拖拽静态方法
+		var dragEle = object['element'], 
+		    handlerEle = object['handler'];
 
 		$(dragEle).fe_draggable({'handler':handlerEle});
 	}
 
-	$.fn.fe_login = function(object){//登录实例化方法
+	$.fn.fe_login = function (object) {//登录实例化方法
 
 		var type = object['type'] === undefined ? 'post' : object['type'];
 		var titleName = object['title'] === undefined ? '请添加系统名称' : object['title'];
@@ -29,12 +29,12 @@
 		var succ_callback = object['success'];
 		var fail_callback = object['failure'];
 
-		$.each(this,function(index,el){
-			createObject().createLoginPage(this,titleName).login(type,page,succ_callback,fail_callback);
+		$.each(this, function (index, el) {
+			createObject().createLoginPage(this, titleName).login(type, page, succ_callback, fail_callback);
 		});
 	}
 
-	$.fe_login = function(object){//登录静态方法
+	$.fe_login = function (object) {//登录静态方法
 
 		var type = object['type'] === undefined ? 'post' : object['type'];
 		var titleName = object['title'] === undefined ? '请添加系统名称' : object['title'];
@@ -43,70 +43,70 @@
 		var fail_callback = object['failure'];
 		var parentEle = object['parent'] === undefined ? 'body' : object['parent'];
 
-		$(parentEle).fe_login({'title':titleName,'type':type,'page':page,'success':succ_callback,'failure':fail_callback});
+		$(parentEle).fe_login({'title':titleName, 'type':type, 'page':page, 'success':succ_callback, 'failure':fail_callback});
 	}
 
-    $.fe_getStyle = function(selector,attr){//获取元素的样式属性值静态方法
+    $.fe_getStyle = function (selector, attr) {//获取元素的样式属性值静态方法
     	return $(selector).fe_getStyle(attr);    	
     }
 
-    $.fn.fe_getStyle = function(attr){//获取元素的样式属性的实例化方法
-    	if(this[0].currentStyle){//兼容IE
+    $.fn.fe_getStyle = function (attr) {//获取元素的样式属性的实例化方法
+    	if(this[0].currentStyle) {//兼容IE
     		return parseFloat(this[0].currentStyle[attr]);
     	}else{
-    		return parseFloat(window.getComputedStyle(this[0],false)[attr]);//兼容W3C
+    		return parseFloat(window.getComputedStyle(this[0], false)[attr]);//兼容W3C
     	}
     }
 
-    $.fe_addTable = function(selector,obj){//添加表格的静态方法
+    $.fe_addTable = function (selector, obj) {//添加表格的静态方法
     	$(selector).fe_addTable(obj);
     }
 
-    $.fn.fe_addTable = function(obj){//添加表格的实例化方法
-    	$.each(this,function(index,el){
+    $.fn.fe_addTable = function (obj) {//添加表格的实例化方法
+    	$.each(this, function (index, el) {
     		var _this = this;
-    		createObject().createTable(_this,obj);
+    		createObject().createTable(_this, obj);
     	});
     }
 
-    $.fe_drawChart = function(selector,obj){//绘制图表的静态方法
+    $.fe_drawChart = function (selector, obj) {//绘制图表的静态方法
     	$(selector).fe_drawChart(obj);
     }
 
-    $.fn.fe_drawChart = function(obj){//绘制图表的实例化方法
-    	$.each(this,function(index, el) {
+    $.fn.fe_drawChart = function (obj) {//绘制图表的实例化方法
+    	$.each(this, function (index, el) {
     		var _this = this;
-    		createObject().drawChart(_this,obj);
+    		createObject().drawChart(_this, obj);
     	});
     }
 
-    $.fe_lockScreen = function(selector,obj){//遮罩锁屏静态方法
+    $.fe_lockScreen = function (selector, obj) {//遮罩锁屏静态方法
     	$(selector).fe_lockScreen(obj);
     }
 
-    $.fn.fe_lockScreen = function(obj){//遮罩锁屏实例化方法
-    	$.each(this,function(index, el) {
+    $.fn.fe_lockScreen = function (obj) {//遮罩锁屏实例化方法
+    	$.each(this, function (index, el) {
     		var _this = this;
     		createObject().popDiv(_this).lockScreen(obj);
     	});
     }
 
-    $.fe_alert = function(str){//弹框警告方法
+    $.fe_alert = function (str) {//弹框警告方法
     	createObject().popDiv('body').alert(str);
     }
 
-    $.fe_confirm = function(str,callback){//弹框确认方法
-    	createObject().popDiv('body').confirm(str,callback);
+    $.fe_confirm = function (str, callback) {//弹框确认方法
+    	createObject().popDiv('body').confirm(str, callback);
     }
 
-    $.fe_addMenu = function(selector,obj){//生成菜单静态方法
+    $.fe_addMenu = function (selector, obj) {//生成菜单静态方法
     	$(selector).fe_addMenu(obj);
     }
 
-    $.fn.fe_addMenu = function(obj){//生成菜单的实例化方法
-    	$.each(this,function(index, el) {
+    $.fn.fe_addMenu = function (obj) {//生成菜单的实例化方法
+    	$.each(this, function (index, el) {
     		var _this = this;
-    		createObject().addMenu(_this,obj,'first');
+    		createObject().addMenu(_this, obj, 'first');
     	});
     }
 
@@ -115,15 +115,15 @@
 /*
 	fore-end对象封装过程
 */
-var createObject = function(){
+var createObject = function () {
 	return new FEObject();
 }
 
-function FEObject(){};
+function  FEObject() {};
 
 FEObject.prototype = {
 	/*拖拽*/
-	drag:function(element,handler){
+	drag:function (element, handler) {
 
 		var _this = this;
 
@@ -132,7 +132,7 @@ FEObject.prototype = {
 		var parentWidth = element.parentNode.nodeName == 'BODY' ? window.innerWidth : element.parentNode.clientWidth;
 		var parentHeight = element.parentNode.nodeName == 'BODY' ? window.innerHeight : element.parentNode.clientHeight;
 
-		handler.onmousedown = function(e){
+		handler.onmousedown = function (e) {
 			var e = e || window.event;
 			e.stopPropagation();
 			e.preventDefault();
@@ -144,25 +144,25 @@ FEObject.prototype = {
 			var e_div_top = e_top_d - selfTop;
 			var e_div_left = e_left_d - selfLeft;
 
-			handler.onmousemove = function(e){
+			handler.onmousemove = function (e) {
 				e.stopPropagation();
 				e.preventDefault();
 				var e_left_m = e.clientX;
 				var e_top_m = e.clientY;
 
-				if(e_left_m < e_div_left){
+				if(e_left_m < e_div_left) {
 					e_left_m = e_div_left;
 				}
 
-				if(e_top_m < e_div_top){
+				if(e_top_m < e_div_top) {
 					e_top_m = e_div_top;
 				}
 
-				if((selfWidth - parentWidth + e_left_m) > e_div_left){
+				if((selfWidth - parentWidth + e_left_m) > e_div_left) {
 					e_left_m = parentWidth - selfWidth + e_div_left;
 				}
 
-				if((selfHeight - parentHeight + e_top_m) > e_div_top){
+				if((selfHeight - parentHeight + e_top_m) > e_div_top) {
 					e_top_m = parentHeight - selfHeight + e_div_top;
 				}
 
@@ -170,7 +170,7 @@ FEObject.prototype = {
 				element.style.left = e_left_m - e_div_left + 'px';
 			}
 
-			handler.onmouseup = function(){
+			handler.onmouseup = function () {
 				e.stopPropagation();
 				e.preventDefault();
 				handler.onmousemove = null;
@@ -179,9 +179,9 @@ FEObject.prototype = {
 		}
 
 		return _this;
-	},
+	}, 
 	/*登录页面创建*/
-	createLoginPage:function(parentEle,titleName){
+	createLoginPage:function (parentEle, titleName) {
 		var _this = this;
 
 		/*创建底层容器*/
@@ -189,8 +189,8 @@ FEObject.prototype = {
 		loginDiv.id = 'loginDiv';
 		loginDiv.className = 'windowBody';
 		parentEle.appendChild(loginDiv);
-		loginDiv.style.left=($.fe_getStyle(parentEle,'width')-$.fe_getStyle(loginDiv,'width'))/2+'px';
-		loginDiv.style.top=($.fe_getStyle(parentEle,'height')-$.fe_getStyle(loginDiv,'height'))/2+'px';
+		loginDiv.style.left=($.fe_getStyle(parentEle, 'width')-$.fe_getStyle(loginDiv, 'width'))/2+'px';
+		loginDiv.style.top=($.fe_getStyle(parentEle, 'height')-$.fe_getStyle(loginDiv, 'height'))/2+'px';
 
 		/*创建题目容器*/
 		var titleDiv = document.createElement('div');
@@ -269,44 +269,44 @@ FEObject.prototype = {
 		formEle.appendChild(resetBtn);
 
 		/*绑定关闭按钮事件*/
-		$('.windowClose').on('click',function(){
+		$('.windowClose').on('click', function () {
 			_this.remove($(this).parent());
 		});
 
 		/*绑定拖动事件*/
 		$.fe_draggable({
-			'element':'#loginDiv',
+			'element':'#loginDiv', 
 			'handler':'#loginDivTitle'
 		});
 
 		return this;
-	},
+	}, 
 	/*登录点击事件*/
-	login:function(type,page,succ_callback,fail_callback){
+	login:function (type, page, succ_callback, fail_callback) {
 
 		var userEle = document.getElementById('inputLoginUsername');
 		var pwdEle = document.getElementById('inputLoginPassword');
 
-		$('#loginReset').on('click',function(){
+		$('#loginReset').on('click', function () {
 			userEle.value = '';
 			pwdEle.value = '';
 		});
 
-		$('input').on('keydown',function(e){
-			if(e.which == 13){//按下回车
-				if(userEle.value == '' || pwdEle.value == ''){
+		$('input').on('keydown', function (e) {
+			if(e.which == 13) {//按下回车
+				if(userEle.value == '' || pwdEle.value == '') {
 					$.fe_alert('用户名或密码为空');
 				}else{
 					var name = userEle.value;
 					var password = pwdEle.value;
 					$.ajax({
-						'type':type,
-						'data':'username='+name+'&password='+password,
-						'url':page,
-						'success':function(msg){
-							if(msg == true){
+						'type':type, 
+						'data':'username='+name+'&password='+password, 
+						'url':page, 
+						'success':function (msg) {
+							if(msg == true) {
 								succ_callback;
-							}else if(msg == false){
+							}else if(msg == false) {
 								fail_callback;
 							}
 
@@ -316,20 +316,20 @@ FEObject.prototype = {
 			}
 		});
 
-		$('#loginSubmit').on('click',function(){
-			if(userEle.value == '' || pwdEle.value == ''){
+		$('#loginSubmit').on('click', function () {
+			if(userEle.value == '' || pwdEle.value == '') {
 				$.fe_alert('用户名或密码为空');
 			}else{
 				var name = userEle.value;
 				var password = pwdEle.value;
 				$.ajax({
-					'type':type,
-					'data':'username='+name+'&password='+password,
-					'url':page,
-					'success':function(msg){
-						if(msg == true){
+					'type':type, 
+					'data':'username='+name+'&password='+password, 
+					'url':page, 
+					'success':function (msg) {
+						if(msg == true) {
 							succ_callback;
-						}else if(msg == false){
+						}else if(msg == false) {
 							fail_callback;
 						}
 
@@ -337,9 +337,9 @@ FEObject.prototype = {
 				});
 			}
 		});
-	},
+	}, 
 	/*生成表格*/
-	createTable:function(container,obj){
+	createTable:function (container, obj) {
 
 		//获取表格信息
 		var tableHeader = obj['tableHeader'];
@@ -357,7 +357,7 @@ FEObject.prototype = {
 		var headTr = document.createElement('tr');
 		tableEle.appendChild(headTr);
 		var headFragment = document.createDocumentFragment();
-		for(var i=0, l=tableHeader.length; i<l; i++){
+		for(var i=0, l=tableHeader.length; i<l; i++) {
 			var headTh = document.createElement('th');
 			headTh.className = 'headTh'+ i;
 			var headText = document.createTextNode(tableHeader[i]);
@@ -370,18 +370,18 @@ FEObject.prototype = {
 		//添加表格内容
 		var contentFragment = document.createDocumentFragment();
 		var oddOrEven = true;
-		for(var key in tableContent){//遍历有几行
+		for(var key in tableContent) {//遍历有几行
 			var contentTr = document.createElement('tr');
 
 			//为行绑定点击事件
-			if(window.addEventListener){//W3C
-				contentTr.addEventListener('click', callback,false);
-			}else if(window.attachEvent){//IE
-				contentTr.attachEvent('onclick',callback);
+			if(window.addEventListener) {//W3C
+				contentTr.addEventListener('click', callback, false);
+			}else if(window.attachEvent) {//IE
+				contentTr.attachEvent('onclick', callback);
 			}
 			
 			//奇偶行换色
-			if(oddOrEven){
+			if(oddOrEven) {
 				contentTr.className = 'evenTr';
 				oddOrEven = false;
 			}else{
@@ -390,7 +390,7 @@ FEObject.prototype = {
 			}
 
 			//遍历有几列
-			for(var i=0, l=tableContent[key].length; i<l; i++){
+			for(var i=0, l=tableContent[key].length; i<l; i++) {
 				var contentTd = document.createElement('td');
 				contentTd.className = 'contentTd' + i;
 				var contentText = document.createTextNode(tableContent[key][i]);
@@ -400,9 +400,9 @@ FEObject.prototype = {
 			contentFragment.appendChild(contentTr);
 		}
 		tableEle.appendChild(contentFragment);
-	},
+	}, 
 	/*绘制图表*/
-	drawChart:function(container,obj){
+	drawChart:function (container, obj) {
 
 		var chartType = obj['type'] === undefined ? 'spline' : obj['type'];
 		var chartTitle = obj['title'] === undefined ? '' : obj['title'];
@@ -412,23 +412,23 @@ FEObject.prototype = {
 		$(container).highcharts({//可根据不同的需求配置此处，不可能写全了满足所有需求的
 			loading:{
 				labelStyle:{
-					top:'1%',
+					top:'1%', 
 					fontWeight:'bold'
 				}
-			},
+			}, 
 			chart:{
-				type:chartType,
+				type:chartType, 
 				zoomType:'x'
-			},
+			}, 
 			title:{
 				text:chartTitle
-			},
+			}, 
 			xAxis:{
 				categories:chartXValue
-			},
+			}, 
 			plotOptions:{
 				series:{
-					allowPointSelect:true,
+					allowPointSelect:true, 
 					marker:{
 						radius:2
 					}
@@ -439,15 +439,15 @@ FEObject.prototype = {
 		/*添加series绘制图表*/
 		var chart = $(container).highcharts();
 
-		if(chart.series.length > 0){
+		if(chart.series.length > 0) {
 			chart.series[0].remove();
 		}else{
 			chart.addSeries({'data':chartSeries});
 		}
 
-	},
+	}, 
 	/*弹框*/
-	popDiv:function(container){
+	popDiv:function (container) {
 		var _this = this;
 
 		var newCoverDiv =document.createElement('div');
@@ -466,15 +466,15 @@ FEObject.prototype = {
 		newNoticeTitleDiv.innerHTML = '提示';
 
 		$.fe_draggable({
-			'element':'#noticeDiv',
+			'element':'#noticeDiv', 
 			'handler':'#noticeTitleDiv'
 		});
 
 		return _this;
 
-	},
+	}, 
 	/*遮罩锁屏*/
-	lockScreen:function(obj){
+	lockScreen:function (obj) {
 		var _this = this;
 
 		var noticeStr = obj['noticeWord'] === undefined ? '请输入提示信息' : obj['noticeWord'];
@@ -485,20 +485,20 @@ FEObject.prototype = {
 		$('#noticeDiv').append(newNoticeContentDiv);
 		newNoticeContentDiv.innerHTML = noticeStr;
 
-		if(isHandle){
+		if(isHandle) {
 			var newNoticeCloseImg = document.createElement('img');
 			newNoticeCloseImg.id = 'noticeCloseImg';
 			newNoticeCloseImg.className = 'windowClose';
 			newNoticeCloseImg.src = 'imagepackage/close-1.png';
 			$('#noticeDiv').append(newNoticeCloseImg);
 
-			$('#noticeCloseImg').on('click',function(){
+			$('#noticeCloseImg').on('click', function () {
 				_this.remove('#noticeCoverLayerDiv');
 			});
 		}
-	},
+	}, 
 	/*弹警告窗*/
-	alert:function(str){
+	alert:function (str) {
 		var _this = this;
 
 		var newNoticeContentDiv = document.createElement('div');
@@ -512,12 +512,12 @@ FEObject.prototype = {
 		buttonDiv.innerHTML = '确认';
 		$('#noticeDiv').append(buttonDiv);
 
-		$(buttonDiv).on('click',function(){
+		$(buttonDiv).on('click', function () {
 			_this.remove('#noticeCoverLayerDiv');
 		});
-	},
+	}, 
 	/*弹确认窗*/
-	confirm:function(str,trueCallback){
+	confirm:function (str, trueCallback) {
 		var _this = this;
 
 		var newNoticeContentDiv = document.createElement('div');
@@ -537,32 +537,32 @@ FEObject.prototype = {
 		buttonDiv2.innerHTML = '取消';
 		$('#noticeDiv').append(buttonDiv2);
 
-		$(buttonDiv1).on('click',function(){
+		$(buttonDiv1).on('click', function () {
 			$('#noticeCoverLayerDiv').remove();
 			trueCallback();
 		});
 
-		$(buttonDiv2).on('click',function(){
+		$(buttonDiv2).on('click', function () {
 			_this.remove('#noticeCoverLayerDiv');
 		});
-	},
+	}, 
 	/*移除元素*/
-	remove:function(selector){
-		$(selector).fadeOut('100', function() {
+	remove:function (selector) {
+		$(selector).fadeOut('100', function () {
 			$(this).remove();
 		});
-	},
+	}, 
 	/*生成菜单*/
-	addMenu:function(selector,obj,which){
+	addMenu:function (selector, obj, which) {
 		var _this = this;
 
-		if(which == 'first'){
+		if(which == 'first') {
 			//生成一个菜单容器
 			var menuContainer = document.createElement('div');
 			menuContainer.id = 'menuDivContent';
 			$(selector).append(menuContainer);
 
-			for(var key in obj){
+			for(var key in obj) {
 				//生成标题容器
 				var menuItemContainer = document.createElement('div');
 				menuItemContainer.className = 'menuSystemDiv';
@@ -578,17 +578,17 @@ FEObject.prototype = {
 				titleDiv.appendChild(spanDiv);
 			}
 
-			$('.menuSystemTitle').on('click',function(){
+			$('.menuSystemTitle').on('click', function () {
 				var childLength = $(this).siblings().length;
-				if(childLength == 0){
+				if(childLength == 0) {
 					var currMenuItem = this.parentNode.firstChild.firstChild.innerHTML;
-					_this.addMenu(this.parentNode,obj[currMenuItem],'second');
+					_this.addMenu(this.parentNode, obj[currMenuItem], 'second');
 				}else{
 					_this.remove($(this).siblings().not('.menuSystemTitle'));
 				}
 			});
-		}else if(which == 'second'){
-			for(var key in obj){
+		}else if(which == 'second') {
+			for(var key in obj) {
 				//生成标题容器
 				var menuItemContainer = document.createElement('div');
 				menuItemContainer.className = 'menuSubSystemDiv';
@@ -601,17 +601,17 @@ FEObject.prototype = {
 
 			}
 
-			$('.menuSubSystemTitle').on('click',function(){
+			$('.menuSubSystemTitle').on('click', function () {
 				var childLength = $(this).siblings().length;
-				if(childLength == 0){
+				if(childLength == 0) {
 					var currMenuItem = this.parentNode.firstChild.innerHTML;
-					_this.addMenu(this.parentNode,obj[currMenuItem]);
+					_this.addMenu(this.parentNode, obj[currMenuItem]);
 				}else{
 					_this.remove($(this).siblings().not('.menuSubSystemTitle'));
 				}
 			});
 		}else{
-			for(var key in obj){
+			for(var key in obj) {
 				//生成标题容器
 				var menuItemContainer = document.createElement('div');
 				menuItemContainer.className = 'menuParaDiv';
